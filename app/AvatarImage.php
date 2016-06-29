a7e9537eee98ae3e57d0e7b23ef19cb167204f30
synt4xy <jarrodnoonan@icloud.com>
2016-06-29 12:36:15 +0100

    Update AvatarImage.php

--- app/AvatarImage.php
+++ app/AvatarImage.php
@@ -6,11 +6,6 @@
  * Project Ended: 27/06/2016
  */
 
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once( 'AvatarImageClass.php' );
 
 new AvatarImage();
